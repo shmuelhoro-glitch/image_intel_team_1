0bb145a496f2e1fb3022818dbb93d2f175c6d4e8
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-03-08 14:08:03 +0200

    add empty lines

--- src/extractor.py
+++ src/extractor.py
@@ -16,26 +16,137 @@ def has_gps(data: dict):
     pass
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def latitude(data: dict):
     pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def longitude(data: dict):
     pass
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def datatime(data: dict):
     pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def camera_make(data: dict):
     pass
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def camera_model(data: dict):
     pass
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def extract_metadata(image_path):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     """
     שולף EXIF מתמונה בודדת.
 
